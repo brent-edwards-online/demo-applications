c81912069e9c91447434d87ba5473451e1811744
brent-edwards-online <brent@brentedwardsonline.com>
2017-07-23 19:30:02 +1000

    Balanced Brackets Solution

---
+++ BalancedBrackets/BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BalancedBrackets
+{
+    public class BalancedBrackets
+    {
+        readonly Regex r = new Regex("(\\[|\\{|\\()");
+
+        public bool Check(string command)
+        {
+            Stack<char> brackets = new Stack<char>();
+
+            foreach(var bracket in command)
+            {
+                if(r.IsMatch("" + bracket) ==  true)
+                {
+                    brackets.Push(bracket);
+                }
+                else
+                {
+                    if(brackets.Count)
+                    var top = brackets.Pop();
+                    switch(top)
+                    {
+                        case '{':
+                            if (bracket != '}')
+                                return false;
+                            break;
+                        case '(':
+                            if (bracket != ')')
+                                return false;
+                            break;
+                        case '[':
+                            if (bracket != ']')
+                                return false;
+                            break;
+                    }
+                }
+            }
+
+            if (brackets.Count != 0)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ BalancedBrackets/BalancedBrackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var data = "]";
+
+
+            var bal = new BalancedBrackets();
+            var result = bal.Check(data);
+            if(result)
+            {
+                Console.WriteLine(data + " is balanced");
+            }
+            else
+            {
+                Console.WriteLine(data + " is not balanced");
+            }
+            Console.ReadLine();
+        }
+    }
+}
